67fb2cf71b1727ca65199f651c5031d78ffe85b0
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-10 01:46:17 +0530

    Add a query to filter and search for artists

---
+++ database/queries/SearchArtists.js
@@ -0,0 +1,27 @@
+const Artist = require('../models/artist');
+
+/**
+ * Searches through the Artist collection
+ * @param {object} criteria An object with a name, age, and yearsActive
+ * @param {string} sortProperty The property to sort the results by
+ * @param {integer} offset How many records to skip in the result set
+ * @param {integer} limit How many records to return in the result set
+ * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ */
+module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+	return Artist.find({})
+				 .sort((`${sortProperty}` : 1))
+				 .skip(offset)
+				 .limit(limit)
+				 .then((artists) => {
+				 	const c = artists.length,
+					
+					return {
+				 		all: [artists],
+				 		count: c,
+				 		offset,
+				 		limti
+
+				 	}
+				 });
+};
