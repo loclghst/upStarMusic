887dfa096bd9e1e3848a1d31645150f61f22bfd2
Manish Kumar Sharma <loclghst@gmail.com>
2018-10-09 22:43:13 +0530

    Add a getAgeRange query

---
+++ database/queries/GetAgeRange.js
@@ -0,0 +1,24 @@
+const Artist = require('../models/artist');
+
+/**
+ * Finds the lowest and highest age of artists in the Artist collection
+ * @return {promise} A promise that resolves with an object
+ * containing the min and max ages, like { min: 16, max: 45 }.
+ */
+module.exports = () => {
+	const minAge =Artist.find({})
+		  			.sort({age: 1})
+		  			.limit(1)
+		  			.then((users) => users[0].age);
+		  			
+	const maxAge = Artist.find({})
+					.sort({age :-1})
+					.limit(1)
+					.then((users) =>users[0].age);
+	
+	return Promise.all([minAge,maxAge])
+				  .then([results] =>{
+				  	return {min: results[0], max: results[1]}
+				  });
+
+};
