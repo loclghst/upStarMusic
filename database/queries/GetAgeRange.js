021701115b47a3ced24dca8b1709ce881d5da7ae
Manish Sharma <loclghst@gmail.com>
2021-03-15 14:04:56 +0530

    Bug fizx

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -6,19 +6,17 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
-	const minAge =Artist.find({})
-		  			.sort({age: 1})
-		  			.limit(1)
-		  			.then((users) => users[0].age);
-		  			
-	const maxAge = Artist.find({})
-					.sort({age :-1})
-					.limit(1)
-					.then((users) =>users[0].age);
-	
-	return Promise.all([minAge,maxAge])
-				  .then([results] =>{
-				  	return {min: results[0], max: results[1]}
-				  });
+  const minAge = Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+    .then((users) => users[0].age);
 
+  const maxAge = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+    .then((users) => users[0].age);
+
+  return Promise.all([minAge, maxAge]).then((results) => {
+    return { min: results[0], max: results[1] };
+  });
 };
